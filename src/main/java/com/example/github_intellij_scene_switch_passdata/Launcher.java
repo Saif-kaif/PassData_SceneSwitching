a48833e1559e359ba94b3f2f6f0774defe23ca3f
Saifuzzaman Kaif <machining1990@gmail.com>
2026-07-28 03:13:03 +0600

    pass Data Switch

---
+++ src/main/java/com/example/github_intellij_scene_switch_passdata/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.github_intellij_scene_switch_passdata;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.github_intellij_scene_switch_passdata {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.github_intellij_scene_switch_passdata to javafx.fxml;
+    exports com.example.github_intellij_scene_switch_passdata;
+}
